b8dd4d6f8d53edb440a9f0f60a3e401ad4d9936c
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-06 12:04:20 +0200

    add didattica

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,11 @@ import phoneLogo from '../images/phone-logo.png';
 import { StaticQuery, graphql } from "gatsby"
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Header from './Header';
-//import RichiestaContatti from './Main/RichiestaContatti';
+import Prodotti from './Main/Prodotti';
+
 //import Didattica from './Main/Didattica';
-//import Prodotti from './Main/Prodotti';
+
+//import RichiestaContatti from './Main/RichiestaContatti';
 import '../styles/index.sass'
 
 const TemplateWrapper = ({ children }) => (
@@ -53,7 +55,16 @@ const TemplateWrapper = ({ children }) => (
         seo={data.datoCmsHome.seoMetaTags}
       />
 <Header />
-        
+<section id="hero">
+          <div className="container">
+            <div id="logo">
+              <img src={tabletLogo} alt="Maieutical Labs" className="md-up-only" />
+              <img src={phoneLogo} alt="Maieutical Labs" className="sm-only" />
+            </div>
+          </div>
+        </section>
+        <Prodotti />
+        <Didattica />
         
 
       {/*
