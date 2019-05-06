c337d7b571f61ed0a820755b095280ef561a63c1
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-06 16:45:13 +0200

    removed works template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Masonry from 'react-masonry-component'
-import Img from 'gatsby-image'
 import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
