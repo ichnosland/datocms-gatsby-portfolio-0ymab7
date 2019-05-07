5e19206a2d922ae44c5b4b00e00369caa1bf3893
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-07 17:49:36 +0200

    create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,28 +4,5 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allDatoCmsProdotti {
-          edges {
-            node {
-              slug
-            }
-          }
-        }
-      }
-    `).then(result => {
-      result.data.allDatoCmsProdotti.edges.map(({ node: work }) => {
-        createPage({
-          path: `works/${work.slug}`,
-          component: path.resolve(`./src/components/Prodotti/index.js`),
-          context: {
-            slug: work.slug,
-          },
-        })
-      })
-      resolve()
-    })
-  })
+  
 }
