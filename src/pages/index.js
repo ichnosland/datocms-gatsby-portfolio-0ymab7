d63cc7a4599134f128fdbf12d8ca56ae561af037
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-07 11:10:09 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
-import injectSaga from 'utils/injectSaga';
-import Layout from 'components/layout';
-import { homepageSendTicketAction } from 'containers/HomePage/actions';
-import { sagasHomepage } from 'containers/HomePage/saga';
+import injectSaga from '../utils/injectSaga';
+import Layout from '../components/layout';
+import { homepageSendTicketAction } from '../containers/HomePage/actions';
+import { sagasHomepage } from '../containers/HomePage/saga';
 
 export class IndexPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
