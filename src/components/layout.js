f6dec09ebf436bb82e9388237b7222baf38ffc2c
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-07 09:27:32 +0200

    removed about page

--- src/components/layout.js
+++ src/components/layout.js
@@ -75,7 +75,7 @@ const TemplateWrapper = ({ children }) => (
       <Prodotti data={data}/>
       <Didattica data={data}/>
       <RichiestaContatti
-          {...this.props.formContatti}
+          {...formContatti}
         />
     </div>
     )}
