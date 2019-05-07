0694f79e80b624ebc7d6d6f228238b9b188fb1ea
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-07 10:15:42 +0200

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -74,7 +74,7 @@ const TemplateWrapper = ({ children }) => (
       </section>
       <Prodotti data={data}/>
       <Didattica data={data}/>
-      <RichiestaContatti {...props}/>
+      <RichiestaContatti {...formContatti}/>
     </div>
     )}
   />
