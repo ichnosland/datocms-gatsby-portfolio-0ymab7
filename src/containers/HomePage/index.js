37c69fc9943e77174f011f18f9bff26360b235ad
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-07 11:46:34 +0200

    homepageview fix

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -79,7 +79,7 @@ const mapStateToProps = (state) => ({
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
 const withSaga = injectSaga({ key: 'homePage', saga: sagasHomepage });
 
-HomePageView = compose(
+const HomePageView = compose(
   withSaga,
   withConnect,
 )(HomePageView);
