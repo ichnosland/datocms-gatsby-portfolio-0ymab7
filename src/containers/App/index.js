8a41ff95036573335a1f857d9fa942ad1fec83fc
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-08 14:49:18 +0200

    removed 404.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,6 +8,12 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from '../HomePage/Loadable';
+import Privacy from '../Privacy';
+import Survey from '../Survey';
+import NotFound from '../NotFoundPage/Loadable';
 import Footer from '../../components/Footer';
 
 export default function App() {
@@ -19,6 +25,12 @@ export default function App() {
       >
         <meta name="description" content="Maieutical Labs è un centro di ricerca privato sulla didattica online e l'e-learning" />
       </Helmet>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/privacy" component={Privacy} />
+        <Route exact path="/survey" component={Survey} />
+        <Route path="" component={NotFound} />
+      </Switch>
       <Footer />
     </div>
   );
