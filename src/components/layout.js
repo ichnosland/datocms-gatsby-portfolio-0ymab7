1cbe9f32dc2c97c1b161910d2e10ff2ea327c758
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-08 10:22:00 +0200

    linting code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,67 +1,63 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { StaticQuery, graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
 import tabletLogo from '../images/tablet-logo.png';
 import phoneLogo from '../images/phone-logo.png';
-
-import { StaticQuery, graphql } from "gatsby"
-import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Header from './Header';
 import Prodotti from './Main/Prodotti';
 import Didattica from './Main/Didattica';
 
 import RichiestaContatti from './Main/RichiestaContatti';
-import '../styles/index.sass'
+import '../styles/index.sass';
 
-const TemplateWrapper = ({ formContatti }) => (
-  <StaticQuery query={graphql`
-    query LayoutQuery
-    {
-      datoCmsSite {
-        globalSeo {
-          siteName
-        }
-        faviconMetaTags {
-          ...GatsbyDatoCmsFaviconMetaTags
-        }
-      }
-      datoCmsHome {
-        seoMetaTags {
-          ...GatsbyDatoCmsSeoMetaTags
-        }
-        introTextNode {
-          childMarkdownRemark {
-            html
+const TemplateWrapper = ({ formContatti }) => {
+  return (<StaticQuery query={graphql`
+      query LayoutQuery
+      {
+        datoCmsSite {
+          globalSeo {
+            siteName
+          }
+          faviconMetaTags {
+            ...GatsbyDatoCmsFaviconMetaTags
           }
         }
-        copyright
-      }
-      allDatoCmsProdotti {
-        edges {
-          node {
-            logo {url} 
-            title
-            description
-            link
+        datoCmsHome {
+          seoMetaTags {
+            ...GatsbyDatoCmsSeoMetaTags
+          }
+          introTextNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+          copyright
+        }
+        allDatoCmsProdotti {
+          edges {
+            node {
+              logo {url} 
+              title
+              description
+              link
+            }
+          }
+        }
+        allDatoCmsDidattica {
+          edges {
+            node {
+              cover {url} 
+              description
+            }
           }
         }
       }
-      allDatoCmsDidattica {
-        edges {
-          node {
-            cover {url} 
-            description
-          }
-        }
-      }
-    }
-  `}
-  render={data => (
-    <div>
-      <HelmetDatoCms
-        favicon={data.datoCmsSite.faviconMetaTags}
-        seo={data.datoCmsHome.seoMetaTags}
-      />
-      
+  `} render={data => (<div>
+      <HelmetDatoCms favicon={data.datoCmsSite.faviconMetaTags} seo={data.datoCmsHome.seoMetaTags} />
+
       <Header />
       <section id="hero">
         <div className="container">
@@ -71,11 +67,21 @@ const TemplateWrapper = ({ formContatti }) => (
           </div>
         </div>
       </section>
-      <Prodotti data={data}/>
-      <Didattica data={data}/>
-    </div>
-    )}
-  />
-)
+      <Prodotti data={data} />
+      <Didattica data={data} />
+      <RichiestaContatti {...formContatti} />
+    </div>)} />);
+}
+
+TemplateWrapper.propTypes = {
+  formContatti: PropTypes.shape({
+    spinner: PropTypes.bool.isRequired,
+    error_message: PropTypes.string.isRequired,
+    ruoli: PropTypes.arrayOf(PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
 
 export default TemplateWrapper
