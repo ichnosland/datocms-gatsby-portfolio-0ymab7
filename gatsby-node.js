7a1741035804a12e33f5df35fb9a2a9f8f7ef4c4
Alessandro Giua <alegiua@AirdiAlessandro-2.lan>
2019-05-08 17:15:05 +0200

    delete pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,10 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
+    createPage({
+      path: `/`,
+      component: path.resolve(`./src/containers/HomePage/index.js`)
+    });
+  
 }
