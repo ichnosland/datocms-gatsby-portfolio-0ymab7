bf8f1fb3fc5b5003284373cec617ce90e2830009
Alessandro Giua <alegiua@AirdiAlessandro-2.lan>
2019-05-08 16:48:19 +0200

    add index in pages folder

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,23 +6,7 @@ import Layout from "../components/layout"
 
 const About = ({ data: { about } }) => (
   <Layout>
-    <article className="sheet">
-      <HelmetDatoCms seo={about.seoMetaTags} />
-      <div className="sheet__inner">
-        <h1 className="sheet__title">{about.title}</h1>
-        <p className="sheet__lead">{about.subtitle}</p>
-        <div className="sheet__gallery">
-          <Img fluid={about.photo.fluid} />
-        </div>
-        <div
-          className="sheet__body"
-          dangerouslySetInnerHTML={{
-            __html: about.bioNode.childMarkdownRemark.html,
-          }}
-        />
-      </div>
-    </article>
-  </Layout>
+   </Layout>
 )
 
 export default About
