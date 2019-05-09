5c2761ead4338df4038b2b86d6d773bf0a01e28b
Alessandro Giua <alegiua@AirdiAlessandro-2.lan>
2019-05-09 09:43:35 +0200

    fix routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,3 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-    createPage({
-      path: `/`,
-      component: path.resolve(`./src/pages/index.js`)
-    }); 
-}
