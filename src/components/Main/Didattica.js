bb0cc20790a8320139b7a5342896cfcd3a86877f
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-13 18:09:14 +0200

    add gdpr banner - lint fix

--- src/components/Main/Didattica.js
+++ src/components/Main/Didattica.js
@@ -2,29 +2,28 @@ import React from 'react';
 
 export default ({ data }) => (
   <div>
-      <section id="chisiamo" className="filler">
-        <h2>Pionieri della didattica innovativa dal 2011</h2>
-        <div className="arrow">
-          <div className="arrow-wrap">
-            <div className="outer-arrow"></div>
-          </div>
+    <section id="chisiamo" className="filler">
+      <h2>Pionieri della didattica innovativa dal 2011</h2>
+      <div className="arrow">
+        <div className="arrow-wrap">
+          <div className="outer-arrow"></div>
         </div>
-      </section>
-      <section>
-        <div className="container row">
-        {data.allDatoCmsDidattica.edges.map(({ node: didattica , index }) => (          
+      </div>
+    </section>
+    <section>
+      <div className="container row">
+        {data.allDatoCmsDidattica.edges.map(({ node: didattica, index }) => (
           <div className="col col-1-2" key={index}>
             <img src={didattica.cover.url} className="col-icn" alt="" />
             <p>{didattica.description}</p>
           </div>
         ))}
-          
+      </div>
+      <div className="arrow">
+        <div className="arrow-wrap">
+          <div className="inner-arrow"></div>
         </div>
-        <div className="arrow">
-          <div className="arrow-wrap">
-            <div className="inner-arrow"></div>
-          </div>
-        </div>
-      </section>
-    </div>
-  )
+      </div>
+    </section>
+  </div>
+);
--- src/components/Main/Prodotti.js
+++ src/components/Main/Prodotti.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 export default ({ data }) => (
-    <section id="prodotti">
-      <div className="arrow top">
-        <div className="arrow-wrap">
-          <div className="outer-arrow"></div>
-          <div className="inner-arrow"></div>
-        </div>
+  <section id="prodotti">
+    <div className="arrow top">
+      <div className="arrow-wrap">
+        <div className="outer-arrow"></div>
+        <div className="inner-arrow"></div>
       </div>
-      
-      <h3>I nostri prodotti</h3>
-      <div className="arrow-wrap small sm-only">
-        <div className="outer-arrow small"></div>
+    </div>
+    <h3>I nostri prodotti</h3>
+    <div className="arrow-wrap small sm-only">
+      <div className="outer-arrow small"></div>
+    </div>
+    <div className="container row row-wrap first">
+      {data.allDatoCmsProdotti.edges.map(({ node: project, index }) => (
+        <a className="col col-1-3 thumb-link" href={project.link} id="itaca" title={project} key={index}>
+          <img className="col-icn small" src={project.logo.url} alt={project.title} />
+          <p>
+            {project.description}
+          </p>
+        </a>
+      ))}
+    </div>
+    <div className="arrow">
+      <div className="arrow-wrap">
+        <div className="inner-arrow"></div>
       </div>
-      <div className="container row row-wrap first">
-        {data.allDatoCmsProdotti.edges.map(({ node: project , index}) => (          
-          <a className="col col-1-3 thumb-link" href={project.link} id="itaca" title={project} key={index}>
-            <img className="col-icn small" src={project.logo.url} alt={project.title} />
-            <p>
-              {project.description}
-            </p>
-          </a>
-        ))}
-      </div>
-      <div className="arrow">
-        <div className="arrow-wrap">
-          <div className="inner-arrow"></div>
-        </div>
-      </div>
-    </section>
-  )
+    </div>
+  </section>
+);
 
