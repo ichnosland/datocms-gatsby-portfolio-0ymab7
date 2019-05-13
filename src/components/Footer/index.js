caf4d82fd8479179f9dda5cede1ef602dd41afd9
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-13 14:15:07 +0200

    dynamic data from datocms in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,8 @@ function Footer({ data }) {
       <p>{data.datoCmsHome.copyright}</p>
       <p className="mini-p" dangerouslySetInnerHTML={{__html:data.datoCmsHome.footer}}></p>
       <CookieConsent>
-      Al fine di offrire il miglior servizio possibile questo sito utilizza cookies tecnici e di terze parti per funzionalità quali l'accesso tramite social network e/o la visualizzazione di media. Se non acconsenti all'utilizzo dei cookie di terze parti, alcune di queste funzionalità potrebbero essere non disponibili. Chiudendo questo banner o proseguendo con la navigazione acconsentirai al loro impiego. Per saperne di più o negare il consenso a tutti o ad alcuni cookie puoi consultare l'informativa sulla privacy.
-
-</CookieConsent>
+        Al fine di offrire il miglior servizio possibile questo sito utilizza cookies tecnici e di terze parti per funzionalità quali l'accesso tramite social network e/o la visualizzazione di media. Se non acconsenti all'utilizzo dei cookie di terze parti, alcune di queste funzionalità potrebbero essere non disponibili. Chiudendo questo banner o proseguendo con la navigazione acconsentirai al loro impiego. Per saperne di più o negare il consenso a tutti o ad alcuni cookie puoi consultare l'informativa sulla privacy.
+      </CookieConsent>
     </footer>
   );
 } 
