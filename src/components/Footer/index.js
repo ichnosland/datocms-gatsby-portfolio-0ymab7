d002736e1a6005b79f4e437e4957631c9d3719d6
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-13 11:24:30 +0200

    dynamic data from datocms in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,7 @@ import CookieConsent from "react-cookie-consent";
 function Footer({ data }) {
   return (
     <footer>
-      <p>{data.datoCmsHome.copyright}</p>
-      <p className="mini-p">{data.datoCmsHome.footerInfo}</p>
+      
       <CookieConsent>
         This website uses cookies to enhance the user experience.
       </CookieConsent>
