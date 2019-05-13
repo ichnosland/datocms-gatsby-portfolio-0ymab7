2c711041a6d53f18c5ba991518d1690ea756c4b2
Alessandro Giua <alegiua@AirdiAlessandro.lan>
2019-05-13 15:50:12 +0200

    footer issue

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-// import CookieConsent from 'react-cookie-consent';
+import CookieConsent from 'react-cookie-consent';
 
 function Footer(prop) {
   return (
     <footer>
-      <p>{JSON.stringify(prop.data)}</p>
+      <p>{JSON.stringify(prop)}</p>
+      <p>{prop.data.datoCmsHome.copyright}</p>
+      <p className="mini-p" dangerouslySetInnerHTML={{ __html: prop.data.datoCmsHome.footer }}></p>
+      <CookieConsent >
+        <p dangerouslySetInnerHTML={{ __html: prop.data.datoCmsHome.cookieBanner }}></p>
+      </CookieConsent>
     </footer>
   );
 }
